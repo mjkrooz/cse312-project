102084f1dbd9505fd448d4ff0862b33aeaba605b
mjkrooz <mjkreuze@buffalo.edu>
2024-02-21 13:34:59 -0500

    Added comments, minor tweak to ordering

--- server.js
+++ server.js
@@ -2,20 +2,26 @@ const express = require('express')
 const app = express();
 const port = 8080;
 
+// Set the `X-Content-Type-Options: nosniff` on all requests. Must come first in this file.
+
 app.use(function(req, res, next) {
 
   res.append('X-Content-Type-Options', 'nosniff');
   next();
 });
 
-app.use(express.static('src/public'));
+// Register `GET /` for the index.
 
 app.get('/', (req, res) => {
 
   res.sendFile(__dirname + '/src/public/index.html');
-
-  res.end();
 });
+
+// Register `src/public` for remaining static routes.
+
+app.use(express.static('src/public'));
+
+// Start the server.
 
 app.listen(port, () => {
   console.log('Listening on port: ' + port);
