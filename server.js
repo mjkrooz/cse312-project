cee58262dab42df2c46591728d84f501c17b4b4a
CodeApprentice43 <nafismortuza43+github@gmail.com>
2024-03-11 09:48:11 -0400

    Added registration form

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = 8080;
 
 // Set the `X-Content-Type-Options: nosniff` on all requests. Must come first in this file.
 
-app.use(function(req, res, next) {
+app.use(function(req, res, next){
 
   res.append('X-Content-Type-Options', 'nosniff');
   next();
@@ -17,12 +17,15 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/src/public/index.html');
 });
 
+app.post('/register',(req,res)=>{
+  console.log(req)
+})
 // Register `src/public` for remaining static routes.
 
 app.use(express.static('src/public'));
 
 // Start the server.
-
+app.use()
 app.listen(port, () => {
   console.log('Listening on port: ' + port);
 });
